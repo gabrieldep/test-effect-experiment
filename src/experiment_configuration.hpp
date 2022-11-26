5bf51d17a9307477022a07083418f226db6824dc
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-11-26 12:28:57 -0300

    Inicial

---
+++ src/experiment_configuration.hpp
@@ -0,0 +1,35 @@
+#ifndef EXPERIMENT_CONFIGURATION_HPP__
+#define EXPERIMENT_CONFIGURATION_HPP__
+#include <Arduino.h>
+
+
+class ExperimentConfiguration{
+    public: 
+        
+        static String current_pair;
+        static String current_gate;
+        static String pair_x_sound;
+        static String pair_x_figure;
+        static String pair_y_sound;
+        static String pair_y_figure;
+        static String pair_z_sound;
+        static String pair_z_figure;
+        static String randomized_gate;
+    
+
+        static void set_current_pair(String current_pair);
+        static void set_pair_x_sound(String new_pair);
+        static void set_pair_y_sound(String new_pair);
+        static void set_pair_z_sound(String new_pair);
+        static void set_pair_x_figure(String new_pair);
+        static void set_pair_y_figure(String new_pair);
+        static void set_pair_z_figure(String new_pair);
+        static void set_current_gate(String gate);            
+        
+        
+        static String get_light_symbol();
+        static String get_sound();
+        static String get_current_gate();
+};
+
+#endif
---
+++ src/stimuli_control.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include "stimuli_control.hpp"
+#include "output_controller.hpp"
+#include "experiment_configuration.hpp"
+
+
+void start_light_stimulus(String received_figures){
+    if (received_figures == "Figure_X"){
+        draw_xis();
+    }
+    else if (received_figures == "Figure_O"){
+        draw_ball(); 
+    }
+    else if (received_figures == "Figure_+"){
+        draw_plus();
+    }
+}
+
+void start_sound_stimulus(String randomized_sound){
+    if (randomized_sound == "100 Hz"){
+        Low_Frequency();
+    }
+    else if (randomized_sound == "750 Hz"){
+        Medium_Frequency();
+    }
+    else if (randomized_sound == "2000Hz"){
+        High_Frequency();
+    }
+}
+
+void execute_paired_stimuli(String figure, String sound){
+    start_light_stimulus(ExperimentConfiguration::get_light_symbol());
+    start_sound_stimulus(ExperimentConfiguration::get_sound());
+    delay(5);
+}
